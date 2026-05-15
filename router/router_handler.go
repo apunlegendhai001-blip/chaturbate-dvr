3e57eb80658ec14e023fff033a1a5ecba4800aa3
apunlegendhai001-blip <apunlegendhai001@gmail.com>
2026-05-15 05:04:00 +0000

    github actions

--- router/router_handler.go
+++ router/router_handler.go
@@ -2,7 +2,10 @@ package router
 
 import (
 	"fmt"
+	"math"
 	"net/http"
+	"os"
+	"path/filepath"
 	"strings"
 	"time"
 
@@ -13,15 +16,46 @@ import (
 
 // IndexData represents the data structure for the index page.
 type IndexData struct {
-	Config   *entity.Config
-	Channels []*entity.ChannelInfo
+	Config     *entity.Config
+	Channels   []*entity.ChannelInfo
+	DiskUsage  string
+}
+
+// getDiskUsage calculates the total size of files in the given directory.
+func getDiskUsage(dir string) string {
+	var totalSize int64
+	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return nil // skip errors
+		}
+		if !info.IsDir() {
+			totalSize += info.Size()
+		}
+		return nil
+	})
+
+	// Format size
+	units := []string{"B", "KB", "MB", "GB", "TB"}
+	size := float64(totalSize)
+	unitIndex := 0
+	for size >= 1024 && unitIndex < len(units)-1 {
+		size /= 1024
+		unitIndex++
+	}
+	return fmt.Sprintf("%.1f %s", size, units[unitIndex])
 }
 
 // Index renders the index page with channel information.
 func Index(c *gin.Context) {
+	dir := "videos"
+	if server.Config.OutputDir != "" {
+		dir = server.Config.OutputDir
+	}
+	diskUsage := getDiskUsage(dir)
 	c.HTML(200, "index.html", &IndexData{
-		Config:   server.Config,
-		Channels: server.Manager.ChannelInfo(),
+		Config:    server.Config,
+		Channels:  server.Manager.ChannelInfo(),
+		DiskUsage: diskUsage,
 	})
 }
 
